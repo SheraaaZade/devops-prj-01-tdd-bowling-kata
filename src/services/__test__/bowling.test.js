669679e79748603756f17a3f9f4921ae5357dfea
aya.elhakkouni <aya.elhakkouni@student.vinci.be>
2022-11-26 01:31:59 +0100

    Trying test : look at the comments

--- src/services/__test__/bowling.test.js
+++ src/services/__test__/bowling.test.js
@@ -45,60 +45,72 @@ describe('should return 0', () => {
     thenResultIs(20);
   });
 
-    // eslint-disable-next-line no-undef
-    it('handles a spare with the appropriate bonus', () => {
-      givenScores([5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-      // eslint-disable-next-line no-use-before-define
-      whenBowling(scores);
-      // eslint-disable-next-line no-use-before-define
-      thenResultIs(16);
-    });
+  // eslint-disable-next-line no-undef
+  it('handles a spare with the appropriate bonus', () => {
+    givenScores([5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    // eslint-disable-next-line no-use-before-define
+    whenBowling(scores);
+    // eslint-disable-next-line no-use-before-define
+    thenResultIs(16);
+  });
 
-    // eslint-disable-next-line no-undef
-    it('handles a strike with the appropriate bonus', () => {
-      givenScores([0, 0, 0, 0, 0, 0, 0, 0, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-      // eslint-disable-next-line no-use-before-define
-      whenBowling(scores);
-      // eslint-disable-next-line no-use-before-define
-      thenResultIs(26);
-    });
+  // eslint-disable-next-line no-undef
+  it('handles a strike with the appropriate bonus', () => {
+    givenScores([0, 0, 0, 0, 0, 0, 0, 0, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    // eslint-disable-next-line no-use-before-define
+    whenBowling(scores);
+    // eslint-disable-next-line no-use-before-define
+    thenResultIs(26);
+  });
 
-     // eslint-disable-next-line no-undef
-     it('result should be 300 as there are only strikes', () => {
-      givenScores(Array(12).fill(10));
-      // eslint-disable-next-line no-use-before-define
-      whenBowling(scores);
-      // eslint-disable-next-line no-use-before-define
-      thenResultIs(300);
-    });
+  // eslint-disable-next-line no-undef
+  it('result should be 300 as there are only strikes', () => {
+    givenScores(Array(12).fill(10));
+    // eslint-disable-next-line no-use-before-define
+    whenBowling(scores);
+    // eslint-disable-next-line no-use-before-define
+    thenResultIs(300);
+  });
 
-     // eslint-disable-next-line no-undef
-     it('result of strik in last round', () => {
-      givenScores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0]);
-      // eslint-disable-next-line no-use-before-define
-      whenBowling(scores);
-      // eslint-disable-next-line no-use-before-define
-      thenResultIs(10);
-    });
+  // eslint-disable-next-line no-undef
+  it('result of strik in last round', () => {
+    givenScores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0]);
+    // eslint-disable-next-line no-use-before-define
+    whenBowling(scores);
+    // eslint-disable-next-line no-use-before-define
+    thenResultIs(10);
+  });
 
-      // eslint-disable-next-line no-undef
-      it('result of spare in last round', () => {
-        givenScores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 0]);
-        // eslint-disable-next-line no-use-before-define
-        whenBowling(scores);
-        // eslint-disable-next-line no-use-before-define
-        thenResultIs(10);
-      });
+  // eslint-disable-next-line no-undef
+  it('result of spare in last round', () => {
+    givenScores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 0]);
+    // eslint-disable-next-line no-use-before-define
+    whenBowling(scores);
+    // eslint-disable-next-line no-use-before-define
+    thenResultIs(10);
+  });
 
-       
-         // eslint-disable-next-line no-undef
-          it('plus de 10 revoie un fall', () => {
-            givenScores([0, 15, 0, 11, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-            // eslint-disable-next-line no-use-before-define
-            whenBowling(scores);
-            // eslint-disable-next-line no-use-before-define
-            thenResultIs(34);
-          });
+
+  // eslint-disable-next-line no-undef
+  it('plus de 10 revoie un fall', () => {
+    givenScores([0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    // eslint-disable-next-line no-use-before-define
+    whenBowling(scores);
+    // eslint-disable-next-line no-use-before-define
+    thenResultIs(undefined);
+  });
+
+  //  // eslint-disable-next-line no-undef
+  //  it('max de lancer', () => {
+  //   givenScores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 10, 5]);
+  //   // eslint-disable-next-line no-use-before-define
+  //   whenBowling(scores);
+  //   // eslint-disable-next-line no-use-before-define
+  //   thenResultIs(undefined);
+  // });
+
+
+
 
 
   function givenScores(customScores) {
--- src/services/bowling.js
+++ src/services/bowling.js
@@ -2,7 +2,11 @@ module.exports = function calculScore(rolls) {
   let score = 0;
   let positionIndex = 0;
   if (rolls === undefined) return undefined;
-  
+
+// const tab = new Array(20);
+  // console.log(tab);                                                     //je cherche à mettre une condition qui definie une taille max de la table et renvoyé undefind 
+  // if( tab.length > 20) return undefined;                                // dans le cas où mon test va tenté d'agrandire la table( avec un strike ou un spare en fin de jeu),
+                                                                          //  à la place d'avoir un failed dans les test on va tenté derenvoyer le resulta undefind .... mais je sais pas comment faire ça!!!!
 
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < rolls.length; i++) {
@@ -10,21 +14,24 @@ module.exports = function calculScore(rolls) {
   }
 
   // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < 10; i++) {
-    const rollOne = rolls[positionIndex];
-    if (rollOne === 10) {
-      score += 10 + rolls[positionIndex + 1] + rolls[positionIndex + 2];
-      // eslint-disable-next-line no-plusplus
-      positionIndex++;
-    } else {
-      // spare
-      const secondRoll = rolls[positionIndex + 1];
-      score += rollOne + secondRoll;
-      if (rollOne + secondRoll === 10) {
-        score += rolls[positionIndex + 2];
+    for (let i = 0; i < 10; i++) {
+
+      const rollOne = rolls[positionIndex];
+    
+      if (rollOne === 10) {
+        score += 10 + rolls[positionIndex + 1] + rolls[positionIndex + 2];
+        // eslint-disable-next-line no-plusplus
+        positionIndex++;
+      } else {
+        // spare
+        const secondRoll = rolls[positionIndex + 1];
+        score += rollOne + secondRoll;
+        if (rollOne + secondRoll === 10) {
+          score += rolls[positionIndex + 2];
+        }
+        positionIndex += 2;
       }
-      positionIndex += 2;
     }
-  }
-  return score;
+    return score;
+
 };
