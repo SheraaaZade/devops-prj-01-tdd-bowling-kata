afa19ac0c44e13c46fc5cb39666136e2f884c0bb
Sheraaa <chehrazad.ouazzani@student.vinci.be>
2022-11-26 21:23:03 +0100

    code: structured and removed unnecessary comments of eslint

--- src/services/__test__/bowling.test.js
+++ src/services/__test__/bowling.test.js
@@ -1,121 +1,89 @@
 const calculScore = require('../bowling');
 
 let result;
-// eslint-disable-next-line no-unused-vars
 let scores;
 
 // eslint-disable-next-line no-undef
-describe('should return 0', () => {
-  // eslint-disable-next-line no-undef
-  it('should fail if the result is not 0 since all of the values equal 0', () => {
-    // eslint-disable-next-line no-use-before-define
-    givenScores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-    // eslint-disable-next-line no-use-before-define
-    whenBowling(scores);
-    // eslint-disable-next-line no-use-before-define
-    thenResultIs(0);
-  });
-
-  // eslint-disable-next-line no-undef
-  it('should fail as the scores have not been provided', () => {
-    // eslint-disable-next-line no-use-before-define
-    givenScores();
-    // eslint-disable-next-line no-use-before-define
-    whenBowling(scores);
-    // eslint-disable-next-line no-use-before-define
-    thenResultIs(undefined);
-  });
-
-  // eslint-disable-next-line no-undef
-  it('should fail as there are negative values in the scores', () => {
-    // eslint-disable-next-line no-use-before-define
-    givenScores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -10, 0, 0, 0, 0, 0, 0, 0, 0]);
-    // eslint-disable-next-line no-use-before-define
-    whenBowling(scores);
-    // eslint-disable-next-line no-use-before-define
-    thenResultIs(undefined);
-  });
-
-  // eslint-disable-next-line no-undef
-  it('should return 20 as each value is equal to one', () => {
-    // eslint-disable-next-line no-use-before-define
-    givenScores(Array(20).fill(1));
-    // eslint-disable-next-line no-use-before-define
-    whenBowling(scores);
-    // eslint-disable-next-line no-use-before-define
-    thenResultIs(20);
-  });
-
-  // eslint-disable-next-line no-undef
-  it('handles a spare with the appropriate bonus', () => {
-    // eslint-disable-next-line no-use-before-define
-    givenScores([5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-    // eslint-disable-next-line no-use-before-define
-    whenBowling(scores);
-    // eslint-disable-next-line no-use-before-define
-    thenResultIs(16);
-  });
-
-  // eslint-disable-next-line no-undef
-  it('handles a strike with the appropriate bonus', () => {
-    // eslint-disable-next-line no-use-before-define
-    givenScores([0, 0, 0, 0, 0, 0, 0, 0, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-    // eslint-disable-next-line no-use-before-define
-    whenBowling(scores);
-    // eslint-disable-next-line no-use-before-define
-    thenResultIs(26);
-  });
-
-  // eslint-disable-next-line no-undef
-  it('result should be 300 as there are only strikes', () => {
-    // eslint-disable-next-line no-use-before-define
-    givenScores(Array(12).fill(10));
-    // eslint-disable-next-line no-use-before-define
-    whenBowling(scores);
-    // eslint-disable-next-line no-use-before-define
-    thenResultIs(300);
-  });
-
-  // eslint-disable-next-line no-undef
-  it('should make a functional strike in the last round', () => {
-    // eslint-disable-next-line no-use-before-define
-    givenScores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 4, 7]);
-    // eslint-disable-next-line no-use-before-define
-    whenBowling(scores);
-    // eslint-disable-next-line no-use-before-define
-    thenResultIs(21);
-  });
-
-  // eslint-disable-next-line no-undef
-  it('should have a functional spare in the last round', () => {
-    // eslint-disable-next-line no-use-before-define
-    givenScores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 8]);
-    // eslint-disable-next-line no-use-before-define
-    whenBowling(scores);
-    // eslint-disable-next-line no-use-before-define
-    thenResultIs(18);
-  });
-
-  // eslint-disable-next-line no-undef
-  it('should fail as incorrect values have been entered', () => {
-    // eslint-disable-next-line no-use-before-define
-    givenScores([0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-    // eslint-disable-next-line no-use-before-define
-    whenBowling(scores);
-    // eslint-disable-next-line no-use-before-define
-    thenResultIs(undefined);
-  });
-
+describe('Bowling test', () => {
   function givenScores(customScores) {
     scores = customScores;
   }
 
-  function whenBowling(_scores) {
-    result = calculScore(_scores);
+  function whenBowling(customScores) {
+    result = calculScore(customScores);
   }
 
   function thenResultIs(expectedResult) {
     // eslint-disable-next-line no-undef
     expect(result).toEqual(expectedResult);
   }
+  // eslint-disable-next-line no-undef
+  it('should fail if the result is not 0 since all of the values equal 0', () => {
+    givenScores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    whenBowling(scores);
+    thenResultIs(0);
+  });
+
+  // eslint-disable-next-line no-undef
+  it('should fail as the scores have not been provided', () => {
+    givenScores();
+    whenBowling(scores);
+    thenResultIs(undefined);
+  });
+
+  // eslint-disable-next-line no-undef
+  it('should fail as there are negative values in the scores', () => {
+    givenScores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -10, 0, 0, 0, 0, 0, 0, 0, 0]);
+    whenBowling(scores);
+    thenResultIs(undefined);
+  });
+
+  // eslint-disable-next-line no-undef
+  it('should return 20 as each value is equal to one', () => {
+    givenScores(Array(20).fill(1));
+    whenBowling(scores);
+    thenResultIs(20);
+  });
+
+  // eslint-disable-next-line no-undef
+  it('handles a spare with the appropriate bonus', () => {
+    givenScores([5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    whenBowling(scores);
+    thenResultIs(16);
+  });
+
+  // eslint-disable-next-line no-undef
+  it('handles a strike with the appropriate bonus', () => {
+    givenScores([0, 0, 0, 0, 0, 0, 0, 0, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    whenBowling(scores);
+    thenResultIs(26);
+  });
+
+  // eslint-disable-next-line no-undef
+  it('result should be 300 as there are only strikes', () => {
+    givenScores(Array(12).fill(10));
+    whenBowling(scores);
+    thenResultIs(300);
+  });
+
+  // eslint-disable-next-line no-undef
+  it('should make a functional strike in the last round', () => {
+    givenScores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 4, 7]);
+    whenBowling(scores);
+    thenResultIs(21);
+  });
+
+  // eslint-disable-next-line no-undef
+  it('should have a functional spare in the last round', () => {
+    givenScores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 8]);
+    whenBowling(scores);
+    thenResultIs(18);
+  });
+
+  // eslint-disable-next-line no-undef
+  it('should fail as incorrect values have been entered', () => {
+    givenScores([0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    whenBowling(scores);
+    thenResultIs(undefined);
+  });
 });
--- src/services/bowling.js
+++ src/services/bowling.js
@@ -1,9 +1,34 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-shadow */
 module.exports = function calculScore(rolls) {
   let score = 0;
   let positionIndex = 0;
 
   if (rolls === undefined) return undefined;
 
+  // eslint-disable-next-line no-shadow
+  function sum(rolls, positionIndex) {
+    return rolls[positionIndex] + rolls[positionIndex + 1];
+  }
+  
+  // eslint-disable-next-line no-shadow
+  function markStrike(rolls, positionIndex) {
+    return 10 + rolls[positionIndex + 1] + rolls[positionIndex + 2];
+  }
+  
+  // eslint-disable-next-line no-shadow
+  function markSpare(rolls, positionIndex) {
+    return 10 + rolls[positionIndex + 2];
+  }
+  
+  // eslint-disable-next-line no-shadow
+  function isStrike(rolls, positionIndex) {
+    return rolls[positionIndex] === 10;
+  }
+  
+  function isSpare(rolls, positionIndex) {
+    return rolls[positionIndex] + rolls[positionIndex + 1] === 10;
+  }
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < rolls.length; i++) {
     if (rolls[i] < 0 || rolls[i] > 10 || rolls[i] === undefined) return undefined;
@@ -11,10 +36,8 @@ module.exports = function calculScore(rolls) {
 
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < 10; i++) {
-    // eslint-disable-next-line no-use-before-define
     if (isStrike(rolls, positionIndex)) {
       // stike
-      // eslint-disable-next-line no-use-before-define
       score += markStrike(rolls, positionIndex);
       // eslint-disable-next-line no-plusplus
       positionIndex++;
@@ -25,9 +48,7 @@ module.exports = function calculScore(rolls) {
     // eslint-disable-next-line no-use-before-define
     const rollScore = sum(rolls, positionIndex);
 
-    // eslint-disable-next-line no-use-before-define
     if (isSpare(rolls, positionIndex)) {
-      // spare
       // eslint-disable-next-line no-use-before-define
       score += markSpare(rolls, positionIndex);
     } else {
@@ -39,23 +60,3 @@ module.exports = function calculScore(rolls) {
   }
   return score;
 };
-
-function sum(rolls, positionIndex) {
-  return rolls[positionIndex] + rolls[positionIndex + 1];
-}
-
-function markStrike(rolls, positionIndex) {
-  return 10 + rolls[positionIndex + 1] + rolls[positionIndex + 2];
-}
-
-function markSpare(rolls, positionIndex) {
-  return 10 + rolls[positionIndex + 2];
-}
-
-function isStrike(rolls, positionIndex) {
-  return rolls[positionIndex] === 10;
-}
-
-function isSpare(rolls, positionIndex) {
-  return rolls[positionIndex] + rolls[positionIndex + 1] === 10;
-}
